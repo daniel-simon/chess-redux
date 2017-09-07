4386a896304f60291615cb7c84d3043171c494b9
Rick O'Halloran <rick.ohalloran@gmail.com>
2017-09-07 11:08:40 -0400

    Changing structure to fit in atomic design.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+  get '/*path', to: 'root#home'
   root :to => "root#home"
 end
