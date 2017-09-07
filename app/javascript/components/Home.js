4a343bf9bb86b3becd76dfa4c3274794ae88516c
Rick O'Halloran <rick.ohalloran@gmail.com>
2017-09-07 10:30:30 -0400

    Updating Home screen.

--- app/javascript/components/Home.js
+++ app/javascript/components/Home.js
@@ -6,9 +6,19 @@ import {push} from 'react-router-redux'
 
 const Home = props => {
   return (
-    <div>
-      Hello World
-      <button className="btn btn-primary">Spectre is working!</button>
+    <div className="container">
+      <div className="columns">
+        <div className="column col-xs-12">
+          <h1>Hello World</h1>
+        </div>
+      </div>
+      <div className="columns">
+        <div className="column col-xs-6">col-xs-6</div>
+        <div className="column col-xs-3">col-xs-3</div>
+        <div className="column col-xs-3">
+          <button className="btn btn-primary">Spectre is working!</button>
+        </div>
+      </div>
     </div>
   )
 }
